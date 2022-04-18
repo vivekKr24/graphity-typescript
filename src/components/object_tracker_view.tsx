594dd7092108cf960f9d5cf3cf11e67a4f73f4c5
vivekKr24 <vivek.kumar.cse20@itbhu.ac.in>
2022-04-19 05:02:44 +0530

    Object tracker View created

--- src/components/object_tracker_view.tsx
+++ src/components/object_tracker_view.tsx
@@ -1,25 +1,41 @@
-// import { useEffect, useState } from "react"
-// import { EventSystem } from "../API/Events/event_system"
+import { useEffect, useState } from "react"
+import { EventSystem } from "../API/Events/event_system"
 
-// export function ObjectTrackerView() {
-//     let [SelectedNode, setSelected]  = useState(-1)
+export function ObjectTrackerView() {
+    let [SelectedNode, setSelectedNode]  = useState(-1)
+    let [SelectedEdge, setSelectedEdge]  = useState([-1, -1])
 
-//     use(() => {
-        
-//     })
+    useEffect(() => {
+        let canvas = document.querySelector("canvas")
+        canvas?.addEventListener('click', (e) => {
+            setTimeout(() => {
 
-//     return (
-//         <>
-//             <table>
-//                 <tr>
-//                     <th>Selected Node</th>
-//                 </tr>
-//                 <tr>
-//                     <td> {EventSystem.GetSelectedNode() === -1 && "NONE"}
-//                         {EventSystem.GetSelectedNode() !== - 1 && EventSystem.GetSelectedNode()}
-//                     </td>
-//                 </tr>
-//             </table>
-//         </>
-//     )
-// }
+                let mouse_x = e.clientX
+                let mouse_y = e.clientY
+                let canvas_x = canvas!!.getBoundingClientRect().x
+                let canvas_y = canvas!!.getBoundingClientRect().y
+                
+                let x_rel = mouse_x - canvas_x
+                let y_rel = mouse_y - canvas_y
+                
+                setSelectedNode(EventSystem.GetSelectedNode())
+                setSelectedEdge(EventSystem.GetSelectedEdge())
+            }, 100)
+        })
+    })
+
+    return (
+        <>
+            <table>
+                <tr>
+                    <th>Selected Node</th>
+                </tr>
+                <tr>
+                    <td> {EventSystem.GetSelectedNode() === -1 && "NONE"}
+                        {EventSystem.GetSelectedNode() !== - 1 && EventSystem.GetSelectedNode()}
+                    </td>
+                </tr>
+            </table>
+        </>
+    )
+}
