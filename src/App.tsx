7bdb1f597ebbb86ee7159c05be45b48602d2ef0a
vivekKr24 <vivek.kumar.cse20@itbhu.ac.in>
2022-04-20 03:54:29 +0530

    basic app design

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Canvas } from './components/canvas';
 import { ObjectTrackerView } from './components/object_tracker_view';
+import { AdjecencyListView } from './components/adj_list_view';
 
 function App() {
   return (
     <div className="App">
-      <Canvas/>
-      <ObjectTrackerView/>
+      <div id='draw-area'>
+        <Canvas/>
+      </div>
+
+      <div id='stat-area'>
+        <ObjectTrackerView/>
+        <AdjecencyListView/>
+      </div>
     </div>
   );
 }
