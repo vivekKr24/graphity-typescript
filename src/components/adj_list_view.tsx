1b309363dee6fcb33e5c4b25ef3a9fc2f9e26f84
vivekKr24 <vivek.kumar.cse20@itbhu.ac.in>
2022-04-19 07:01:20 +0530

    Basic adjecency list view created

---
+++ src/components/adj_list_view.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useInsertionEffect, useState } from "react"
+import { EventSystem } from "../API/Events/event_system"
+import { ObjectTracker } from "../API/Events/object_tracker"
+
+interface node_edge_list {
+    node_id: number
+    edge_list: number[]
+}
+
+function NodeList( {node_id, edge_list}: node_edge_list ) {
+    let [edge_list_, setEdgeList] = useState(edge_list)
+
+    useEffect(() => {
+        let canvas = document.querySelector("canvas")
+        setEdgeList(ObjectTracker.GetAdjList()[node_id])
+
+        canvas!!.addEventListener('mousemove', (e) => {
+            let mouse_x = e.clientX
+            let mouse_y = e.clientY
+            let canvas_x = canvas!!.getBoundingClientRect().x
+            let canvas_y = canvas!!.getBoundingClientRect().y
+            
+            let x_rel = mouse_x - canvas_x
+            let y_rel = mouse_y - canvas_y
+            
+            setEdgeList(ObjectTracker.GetAdjList()[node_id])
+        })
+    })
+
+
+    return (
+        <>
+            <tr>
+                <td>
+                    { node_id } {"=>"} 
+                </td>
+                <td>
+                    { edge_list_ }
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export function AdjecencyListView() {
+    let [adjList, setAdjList] = useState(ObjectTracker.GetAdjList())
+
+    useEffect(() => {
+        let canvas = document.querySelector("canvas")
+        setAdjList(ObjectTracker.GetAdjList())
+
+        canvas!!.addEventListener('mousemove', (e) => {
+            let mouse_x = e.clientX
+            let mouse_y = e.clientY
+            let canvas_x = canvas!!.getBoundingClientRect().x
+            let canvas_y = canvas!!.getBoundingClientRect().y
+            
+            let x_rel = mouse_x - canvas_x
+            let y_rel = mouse_y - canvas_y
+            
+            setAdjList(ObjectTracker.GetAdjList())
+        })
+    })
+
+    let list: JSX.Element[] = []
+
+    adjList.forEach((edge_list, node) => {
+        list = [...list, <NodeList node_id={ node } edge_list = {edge_list}/>]
+    })
+
+    return (
+        <>
+            {list}
+        </>
+    )
+}
